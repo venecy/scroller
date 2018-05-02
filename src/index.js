c27c17822deae0d93d81fe04e375cd7b633fdade
gulewei <gu.lewei@qq.com>
2018-05-02 13:06:22 +0800

    update bundle

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import Animate from './animate'
 import Scroller from './scroller'
-import EasyScroller from './easy-scroller'
 
 export default Scroller
 
 export {
-  Animate,
-  EasyScroller
+  Animate
 }
